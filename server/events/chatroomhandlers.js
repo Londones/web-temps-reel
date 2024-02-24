09cd9e2eb50e8b94dc119355a41c678886e751b1
Bah Awa <bah.awaa@gmail.com>
2024-02-24 03:56:40 +0100

    compareArrays function

--- server/events/chatroomhandlers.js
+++ server/events/chatroomhandlers.js
@@ -44,6 +44,18 @@ const handleDisconnect = (socket, io) => () => {
   });
 };
 
+const compareArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const answerChecker = (questions, message) => {
   const messageToLowerCase = message.toLowerCase();
   const userAnswer = messageToLowerCase.split(" ");
